88cbeb627a2b155841b4ef228e040a8c3777ad02
yuming0729 <84842532+yuming0729@users.noreply.github.com>
2023-08-02 11:07:23 +0800

    1
    1

---
+++ js/ajax.js
@@ -0,0 +1,17 @@
+Vue.createApp({
+    data:function(){
+        return{
+            data:'小名'
+        }
+    },
+    methods:{
+
+    },
+    mounted: function() {
+      
+    }
+
+
+
+
+}).mount('#app')
